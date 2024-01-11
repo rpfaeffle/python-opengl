decdfc0a48b8f6704070cbfcf4a2f46aa42805c4
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-01-11 12:30:31 +0100

    feat: restrict player movement

--- src/pong.py
+++ src/pong.py
@@ -4,6 +4,9 @@ from core.context import WindowContext
 from components.div import Div
 from enum import Enum
 
+PLAYER_DIMENSIONS = (10, 100)
+BORDER_WIDTH = 15
+
 class Direction(Enum):
   Up = 0
   Down = 1
@@ -11,11 +14,9 @@ class Direction(Enum):
 
 class Pong(Render):
     def __init__(self, cx: WindowContext):
-        self.border_width = 15
-        self.player = Player()
+        self.player = Player(cx)
         cx.input.register(b'w', 0, lambda: self.player.move(Direction.Up))
         cx.input.register(b's', 0, lambda: self.player.move(Direction.Down))
-        setattr(cx, 'player', {})
 
     @staticmethod
     def new(cx: WindowContext):
@@ -25,28 +26,33 @@ class Pong(Render):
         return Div().child(
             Div() \
                 .set_width(cx.width) \
-                .set_height(self.border_width)
+                .set_height(BORDER_WIDTH)
         ).child(
             Div() \
                 .set_width(cx.width) \
-                .set_height(self.border_width) \
-                .set_y(cx.height - self.border_width)
+                .set_height(BORDER_WIDTH) \
+                .set_y(cx.height - BORDER_WIDTH)
         ).child(self.player)
 
 
 class Player(Component):
-    def __init__(self):
+    def __init__(self, cx: WindowContext):
         super().__init__()
-        self.y = 240 - 50
+        self.maximum_height = cx.height - BORDER_WIDTH - PLAYER_DIMENSIONS[1] - 10
+        self.y = (cx.height - PLAYER_DIMENSIONS[1]) // 2
 
     def move(self, direction: Direction):
-        if direction == Direction.Up:
+        if direction == Direction.Up and self.y < self.maximum_height:
             self.y += 10
-        elif direction == Direction.Down:
+        elif direction == Direction.Down and self.y > BORDER_WIDTH + 10:
             self.y -= 10
 
     def render(self, cx):
-        return Div().set_width(10).set_height(100).set_y(self.y).set_x(cx.width - 10 - 15)
+        return Div().set_width(
+          PLAYER_DIMENSIONS[0]
+        ).set_height(
+          PLAYER_DIMENSIONS[1]
+        ).set_y(self.y).set_x(cx.width - 10 - 15)
 
 
 if __name__ == '__main__':
