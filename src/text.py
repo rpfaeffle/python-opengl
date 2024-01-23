d313302b68b00f24aebba74269fff3bc9f648a1c
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-01-23 15:41:59 +0100

    another try to get text rendering done

--- src/text.py
+++ src/text.py
@@ -14,11 +14,14 @@ class TextRenderer:
 
     def load_chars(self):
         for i in range(128):
-            self.face.load_char(chr(i), freetype.FT_LOAD_RENDER | freetype.FT_LOAD_FORCE_AUTOHINT)
-            bitmap = self.face.glyph.bitmap
-            glNewList(self.list_base+i, GL_COMPILE)
-            glBitmap(bitmap.width, bitmap.rows, 0, 0, self.face.glyph.advance.x >> 6, 0, bitmap.buffer)
-            glEndList()
+          self.face.load_char(chr(i), freetype.FT_LOAD_RENDER | freetype.FT_LOAD_FORCE_AUTOHINT)
+          bitmap = self.face.glyph.bitmap
+          glNewList(self.list_base+i, GL_COMPILE)
+          glBitmap(bitmap.width, bitmap.rows, 0, 0, self.face.glyph.advance.x >> 6, 0, bitmap.buffer)
+          glEndList()
+          error = glGetError()
+          if error != GL_NO_ERROR:
+              print("Error", error)
 
     def render_text(self, text, x, y):
         glRasterPos2f(x, y)
@@ -31,6 +34,7 @@ class Text(Render):
     self.text = TextRenderer("./src/Arial.ttf", 16)
 
   def render(self, cx) -> None:
+    glColor3f(1.0, 1.0, 1.0)
     glEnable(GL_TEXTURE_2D)
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
