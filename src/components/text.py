d5aa95d71bcb03935d2b3529e0ce082b13a7ac67
rpfaeffle <67913738+rpfaeffle@users.noreply.github.com>
2024-02-08 08:36:35 +0100

    feat: add support for text color

--- src/components/text.py
+++ src/components/text.py
@@ -1,3 +1,4 @@
+import OpenGL.GL as gl
 from core.component import Component
 from core.context import WindowContext
 from enum import Enum
@@ -10,6 +11,7 @@ class Text(Component):
     def __init__(self, text: str):
         self.text = text
         self.alignment = Alignment.Left
+        self.color = (1, 1, 1, 1)
 
     def render(self, cx: WindowContext):
         x, y = 0, 0
@@ -17,7 +19,12 @@ class Text(Component):
         if self.alignment == Alignment.Center:
             x = -((cx.font.width * len(self.text)) / 2)
 
+        gl.glColor4f(*self.color)
         cx.font.draw(self.text, x, y)
+
+    def set_color(self, color: tuple[float, float, float, float]):
+        self.color = color
+        return self
 
     def set_alignment(self, alignment: Alignment):
         self.alignment = alignment
